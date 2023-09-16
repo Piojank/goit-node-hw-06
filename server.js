6ae9baac3eb6a042f96134c623bd710ac669612d
piojank <p.jankowski931026@gmail.com>
2023-09-16 19:57:36 +0200

    initialize repository

---
+++ server.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const path = require('path');
+require("dotenv").config();
+const createFolderIfNotExist = require("./middlewares/multer");
+mongoose.Promise = global.Promise;
+
+const publicPath = path.join(process.cwd(), "public");
+const uploadDir = path.join(process.cwd(), "public/tmp");
+const storeImage = path.join(process.cwd(), "public/avatars");
+
+const PORT = process.env.PORT || 3000;
+const uriDb = process.env.DB_HOST;
+
+const connection = mongoose.connect(uriDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME,
+});
+
+connection
+    .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () => {
+            createFolderIfNotExist(publicPath);
+            createFolderIfNotExist(uploadDir);
+            createFolderIfNotExist(storeImage);
+            console.log(`App listens on port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error(`Error while establishing connection: [${error}]`);
+        process.exit(1);
+    });
